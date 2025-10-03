3fbd098bc657b95185ec23914d05e0d8c7fee952
Syed Huzaif <syedhuzaif199@gmail.com>
2025-10-03 18:49:54 +0530

    [FIX] calculate the number of blank spaces based on the number of days passed

--- calendar.go
+++ calendar.go
@@ -28,6 +28,14 @@ func resetStyleAndColors() {
 	fmt.Printf("\x1B[0m")
 }
 
+func mapBtwZeroAndSeven(num int) int {
+	for num < 0 {
+		num = 7 - num
+	}
+
+	return num % 7
+}
+
 func main() {
 	weeksVeryShort := []string{
 		"Su",
@@ -57,11 +65,10 @@ func main() {
 		"Saturday",
 	}
 
-	startsWithMonday := false
 	const (
-		veryShortWeekShortness = 4 // long are at most 7 charters long... add 2 extra characters
-		shortWeekShortness     = 5 // very short are 2 charters long... add 2 extra characters
-		longWeekShortness      = 9 // short are 3 charters long... add 2 extra characters
+		veryShortWeekShortness = 4 // very short are 2 charters long... add 2 extra characters
+		shortWeekShortness     = 5 // short are 3 charters long... add 2 extra characters
+		longWeekShortness      = 11 // long are at most 9 charters long... add 2 extra characters
 	)
 	weekShortness := shortWeekShortness
 	weeks := weeksLong
@@ -70,44 +77,39 @@ func main() {
 	} else if weekShortness == shortWeekShortness {
 		weeks = weeksShort
 	}
-	weeksLen := 0
-	for _, week := range weeks {
-		weeksLen += len(week)
-		weeksLen += 2
-	}
-	weeksLen -= 2
+	weeksLen := 7 * weekShortness - 2
 
+	startsWithMonday := false
 	dayOffset := 0
 	if startsWithMonday {
 		dayOffset = 1
 	}
 
 	currentTime := time.Now()
-	// timefmt := currentTime.Format("12:35:25 AM");
-	// format : 12 hour
 	timefmt := currentTime.Format("03:04:05 PM")
 	monthYear := fmt.Sprintf("%s %d", currentTime.Month(), currentTime.Year())
-	formatStr := fmt.Sprintf("%%%ds", (weeksLen+len(monthYear))/2)
+	
+	fmt.Println(strings.Repeat("-", weeksLen))
+	fmt.Println()
+	fmt.Printf("%*s\n", (weeksLen+len(monthYear))/2, monthYear)
+	fmt.Printf("%*s\n", (weeksLen+len(monthYear))/2, timefmt)
+	fmt.Println()
+	fmt.Println(strings.Repeat("-", weeksLen))
+	
 	currentWeekDay := int(currentTime.Weekday())
-
-	fmt.Println(strings.Repeat("-", weeksLen))
-	fmt.Println()
-	fmt.Printf(formatStr+"\n", monthYear)
-	fmt.Printf(formatStr+"\n", timefmt)
-	fmt.Println()
-	fmt.Println(strings.Repeat("-", weeksLen))
-	formatStr = fmt.Sprintf("%%-%ds", weekShortness-2)
 	for i, _ := range weeks {
 		idx := (i + dayOffset) % 7
 		if idx == currentWeekDay {
 			setRGBBackground(255, 255, 255)
 			setRGBForeground(0, 0, 0)
 		}
-		fmt.Printf(formatStr, weeks[idx])
+		fmt.Printf("%-*s", weekShortness-2 ,weeks[idx])
 		resetStyleAndColors()
 		fmt.Printf("  ")
 	}
 	fmt.Println()
+	fmt.Println()
+
 	dayCount := 30 + (int(currentTime.Month())+1)%2
 	if currentTime.Month() == time.February {
 		dayCount -= 3
@@ -117,23 +119,25 @@ func main() {
 	}
 
 	
-	for i := 0; i < currentWeekDay; i++ {
-		fmt.Printf(formatStr, "")
-		fmt.Printf("  ")
+	daysPassed := int(currentTime.AddDate(0, 0, -currentTime.Day() + 1).Weekday()) - dayOffset
+	daysPassed = mapBtwZeroAndSeven(daysPassed)
+	for range daysPassed {
+		fmt.Printf("%-*s  ", weekShortness-2, "")
 	}
 
-	for i := 0; i < dayCount; i++ {
-		if (currentWeekDay + i) % 7 == 0 {
+	for i := range dayCount {
+		if (daysPassed + i) % 7 == 0 {
 			fmt.Println()
 		}
 		if i + 1 == currentTime.Day() {
 			setRGBBackground(255, 255, 255)
 			setRGBForeground(0, 0, 0)
 		}
-		fmt.Printf(formatStr, fmt.Sprintf("%d", i+1))
+		fmt.Printf("%-*s", weekShortness-2, fmt.Sprintf("%d", i+1))
 		resetStyleAndColors()
 		fmt.Printf("  ")
 	}
 
-
+	fmt.Println()
+	fmt.Println()
 }
